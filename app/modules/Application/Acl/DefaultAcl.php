bc42a43e4746d6a3fb3a976fec5d2bcee5e4023d
Zike <zhzike@gmail.com>
2021-09-16 12:22:01 -0400

    makes roles' Inheritance automatically.
    Makes roles' Inheritance automatically.

--- app/modules/Application/Acl/DefaultAcl.php
+++ app/modules/Application/Acl/DefaultAcl.php
@@ -8,9 +8,13 @@
 
 namespace Application\Acl;
 
+use  Admin\Model\AdminUser;
+
 class DefaultAcl extends \Phalcon\Acl\Adapter\Memory
 {
 
+    private $front_roles = ['guest','member'];
+    
     public function __construct()
     {
         parent::__construct();
@@ -20,27 +24,19 @@ class DefaultAcl extends \Phalcon\Acl\Adapter\Memory
         /**
          * Full list of Roles
          */
-        $roles = [];
-        $roles['guest'] = new \Phalcon\Acl\Role('guest', 'Guest');
-        $roles['member'] = new \Phalcon\Acl\Role('member', 'Member');
-
-        $roles['journalist'] = new \Phalcon\Acl\Role('journalist', 'Journalist');
-        $roles['editor'] = new \Phalcon\Acl\Role('editor', 'Journalist');
-        $roles['admin'] = new \Phalcon\Acl\Role('admin', 'Admin');
-
-        /**
-         * Frontend roles
-         */
-        $this->addRole($roles['guest']);
-        $this->addRole($roles['member'], $roles['guest']);
-
-        /**
-         * Backend roles
-         */
-        $this->addRole($roles['journalist'], $roles['guest']);
-        $this->addRole($roles['editor'], $roles['journalist']);
-        $this->addRole($roles['admin']);
-
+         $roles = [];
+        $available_roles = array_merge($this->front_roles, AdminUser::$roles);
+        foreach ($available_roles as as $role_key => $role) {
+            $roles[$role]  = new \Phalcon\Acl\Role($role, ucfirst($role));
+            if ($role == 'guest') {
+                $this->addRole($roles[$role]);
+            } elseif (in_array($role,['member','journalist'])) {
+                $this->addRole($roles[$role], $roles['guest');
+            }else {
+                $this->addRole($roles[$role], $roles[$available_roles[$role_key - 1]]);
+            }
+        }
+  
         /**
          * Include resources permissions list from file /app/config/acl.php
          */
@@ -48,11 +44,7 @@ class DefaultAcl extends \Phalcon\Acl\Adapter\Memory
 
         foreach ($resources as $roles_resources) {
             foreach ($roles_resources as $resource => $actions) {
-                $registerActions = '*';
-                if (is_array($actions)) {
-                    $registerActions = $actions;
-                }
-                $this->addResource(new \Phalcon\Acl\Resource($resource), $registerActions);
+                $this->addResource(new \Phalcon\Acl\Resource($resource), $actions);
             }
         }
 
@@ -67,15 +59,9 @@ class DefaultAcl extends \Phalcon\Acl\Adapter\Memory
         foreach ($roles as $k => $role) {
             $user_resource = $resources[$k];
             foreach ($user_resource as $roles_resources => $method) {
-                if ($method == '*') {
-                    $this->allow($k, $roles_resources, '*');
-                } else {
-                    $this->allow($k, $roles_resources, $method);
-                }
-
+                $this->allow($k, $roles_resources, $method);
             }
         }
-
     }
 
 }
